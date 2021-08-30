ce361ca4bdcc53f29fcf3942b11c0a4b13153a1f
forestforgeron <forestforgeron@gmail.com>
2021-08-30 22:14:15 +0300

    created page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="shop-page">
+    <Header />
+    <Shop />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/Header.vue";
+import Shop from "./components/Shop.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Shop,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap");
+
+html,
+body,
+div,
+p,
+ul,
+li,
+button,
+input,
+textarea,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+  padding: 0;
+  border-width: 0;
+}
+
+html {
+  scroll-behavior: smooth;
+
+  font-family: "Roboto", Arial, sans-serif;
+  font-size: 16px;
+}
+
+button {
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+
+a {
+  text-decoration: none;
+}
+
+li {
+  list-style: none;
+}
+
+.shop-page {
+  height: 100vh;
+
+  overflow: hidden;
+
+  background: no-repeat center url("assets/background-main.png");
+  background-size: cover;
 }
 </style>
